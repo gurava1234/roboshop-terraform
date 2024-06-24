013ca2b2671de6e0e4bd3c27507bdab3c2bc1917
gurava1234 <bachuguravareddy47@gmail.com>
2024-06-24 11:37:37 +0530

    first commit

---
+++ 03-vpn/variables.tf
@@ -0,0 +1,15 @@
+variable "common_tags" {
+    default = {
+        project = "roboshop"
+        environment = "dev"
+        terraform = "true"
+    }
+}
+
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
---
+++ 04-ec2/variables.tf
@@ -0,0 +1,18 @@
+variable "common_tags" {
+    default = {
+        project = "roboshop"
+        environment = "dev"
+        terraform = "true"
+    }
+}
+
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+variable "zone_name" {
+    default = "daws76s.online"
+}
